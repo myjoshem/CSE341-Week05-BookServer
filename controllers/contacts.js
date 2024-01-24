c0787a345ae749df3552a9430efecc7e25559199
Michelle Markham <michellejmarkham@gmail.com>
2024-01-23 23:07:06 -0700

    some changes, all seems to be working now 🙏🏼

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -16,12 +16,36 @@ async function getContacts(req, res) {
   }
 }
 
+/* const getOne = async (req, res) => {
+  try {
+    const db = await mongodb.getDb();
+    const userId = req.params.id;
+
+    // Validate userId format
+    if (!/^[0-9a-fA-F]{24}$/.test(userId)) {
+      return res.status(400).json({ error: 'Invalid userId format' });
+    }
+
+    const result = await db.collection('contacts')
+      .findOne({ _id: new ObjectId(userId) });
+
+    if (!result) {
+      res.status(404).json({ error: 'Contact not found' });
+    } else {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(result);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}; */
+
 const getOne = async (req, res) => {
   try {
     const db = await mongodb.getDb(); // Use the getDb function
     const userId = new ObjectId(req.params.id);
-    const result = await db.collection('contacts')
-      .findOne({ _id: userId });
+    const result = await db.collection('contacts').findOne({ _id: userId });
 
     if (!result) {
       res.status(404).json({ error: 'Contact not found' });
