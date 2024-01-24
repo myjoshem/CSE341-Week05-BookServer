a6634929c3c15bc1d4e00c8324a2ace7d9d8500a
Michelle Markham <michellejmarkham@gmail.com>
2024-01-24 00:54:38 -0700

    added PUT, POST, AND DELETE requests to database

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,17 +2,21 @@
 
 const express = require('express');
 const router = express.Router();
-
-// Methods for communication
 const contactsMethod = require('../controllers/contacts');
 
 // Gets one contact
 router.get('/:id', contactsMethod.getOne);
 
-// Local json contacts
-//router.get('/contacts', contactsMethod.getLocalContacts);
-
 // Gets all contacts
 router.get('/', contactsMethod.getContacts);
+
+// Create a new contact
+router.post('/', contactsMethod.createContact);
+
+// Update a contact
+router.put('/:id', contactsMethod.updateContact);
+
+// Delete a contact
+router.delete('/:id', contactsMethod.deleteContact);
 
 module.exports = router;
